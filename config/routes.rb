586414389f996efab62e65c897792a86f14dc626
Donnie Propst <donniepropst@gmail.com>
2021-04-22 23:33:23 -0400

    add react scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  # This needs to be the last route in this file!
+  match '*path', to: 'pages#home', via: :all
 end
