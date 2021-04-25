8dc10f5e6b571c42b3092574c8d63b1600293ad5
krakatorange <maleks1@umbc.edu>
2021-04-25 13:37:50 -0400

    Update index.jsx

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "../components/home";
+import Home from "../components/Home";
 
 export default (
   <Router>
