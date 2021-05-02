3c617ea9a632f74ad46f5710dd37686cfbd2b9f0
Misha <mikhail.aleksandrov@nvc-inc.net>
2021-05-02 18:18:16 -0400

    splash made static

--- app/javascript/components/Splash.jsx
+++ app/javascript/components/Splash.jsx
@@ -21,9 +21,9 @@ export default function Splash(){
   setTimeout(() => setToHome(true), 2000)
   setTimeout(() => setToHomeBrowser(true))
 
-  return (
-    <>
-            <BrowserView>
+  /*
+
+  <BrowserView>
             {toHomeBrowser ? <Redirect to="/create" /> : <></>}
             </BrowserView>
             <MobileView>
@@ -39,5 +39,25 @@ export default function Splash(){
                 </div>
               </div>
             </MobileView>
+
+  */
+
+            /*
+            {toHome ? <Redirect to="/create" /> : <></>}
+            */
+
+  return (
+    <>
+      
+      <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+        <div className="jumbotron jumbotron-fluid bg-transparent">
+          <div className="container secondary-color">
+            <h1 className="display-4">Rumineat</h1>
+            <p className="lead">
+              AI Powered Sentiment Analysis For Your Group's Next Meal.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
 )};
