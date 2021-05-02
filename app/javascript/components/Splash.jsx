f1f56ab06eca23c544f7d87bc8bf241a422154ff
Misha <mikhail.aleksandrov@nvc-inc.net>
2021-05-02 16:13:20 -0400

    splash redirect

--- app/javascript/components/Splash.jsx
+++ app/javascript/components/Splash.jsx
@@ -1,16 +1,29 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, Redirect } from "react-router-dom";
 
 export default function Splash(){
+  const [toHome, setToHome] = useState(false);
+
+  useEffect(() => {
+    return () => {
+      setToHome({});
+    };
+  }, []);
+
+  setTimeout(() => setToHome(true), 2000)
+
   return (
-  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-    <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">Rumineat</h1>
-        <p className="lead">
-          AI Powered Sentiment Analysis For Your Group's Next Meal.
-        </p>
+    <>
+      {toHome ? <Redirect to="/create" /> : <></>}
+      <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+        <div className="jumbotron jumbotron-fluid bg-transparent">
+          <div className="container secondary-color">
+            <h1 className="display-4">Rumineat</h1>
+            <p className="lead">
+              AI Powered Sentiment Analysis For Your Group's Next Meal.
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    </>
 )};
