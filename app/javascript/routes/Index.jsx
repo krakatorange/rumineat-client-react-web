aaae2258200fef2fdd278eec8d1846fd2fa2d626
Misha <mikhail.aleksandrov@nvc-inc.net>
2021-05-02 14:17:29 -0400

    update home

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SessionCreate from "../components/SessionCreate";
+import Home from "../components/Home";
 
 export default (
   <Router>
     <Switch>
       <Route path="/test" exact component={SessionCreate} />
+      <Route path="/splash" exact component={Home} />
     </Switch>
   </Router>
 );
