5a84bc96b2f11e1f24d87f67787a25240fac49c2
Misha <mikhail.aleksandrov@nvc-inc.net>
2021-05-05 03:04:28 -0400

    updated map

--- app/javascript/components/SessionCreate.jsx
+++ app/javascript/components/SessionCreate.jsx
@@ -11,24 +11,26 @@ function SessionCreate() {
     const [buttonDisabled, setButtonDisabled] = useState(false);
     const [rangeValue, setRangeValue] = useState(5);
     const [priceLevel, setPriceLevel] = useState('$$');
+    const [currentLocation, setCurrentLocation] = useState({});
+    const [markerCenter, setMarkerCenter] = useState(null);
 
     useEffect(() => {
         console.log("useEffect items loaded");
-    }, []);
+        getLocation();
+    }, [currentLocation]);
 
     function success(position) {
         console.log('Geolocation received!');
         console.log(position);
-        createUserAndSession(position.coords.latitude, position.coords.longitude);
-    };
-    function tryAPIGeolocation() {
-        console.log("also here");
-        fetch(GOOGLE_MAPS_BASE_URI + GOOGLE_MAPS_API_KEY, {
-            method: "POST",
-        }).then = (response) => {
-            success({coords: {latitude: response.location.lat, longitude: response.location.lng}})
-            console.log({coords: {latitude: response.location.lat, longitude: response.location.lng}});
+
+        let data = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
         }
+
+        setCurrentLocation({...currentLocation, ...data})
+
+        console.log(currentLocation);
     };
 
     function error() {
@@ -37,19 +39,28 @@ function SessionCreate() {
     };
 
     async function getLocation() {
-        setButtonDisabled(true);
-        console.log('here!!');
         if (!navigator.geolocation) {
             console.log('Geolocation isnt supported on this device'); //#TODO: show some sort of toast
         } else {
             console.log('Geolocator get coords');
-            navigator.geolocation.getCurrentPosition(success, error);
-            console.log('After geolocator should have received coords');
+            navigator.geolocation.getCurrentPosition(success, error, { enableHighAccuracy: true });
+        }
+    };
+
+    function tryAPIGeolocation() {
+        console.log("Starting API GEO");
+        fetch(GOOGLE_MAPS_BASE_URI + GOOGLE_MAPS_API_KEY, {
+            method: "POST",
+        }).then = (response) => {
+            console.log("hello");
+            success({lat: response.location.lat, lng: response.location.lng})
         }
     };
 
     async function createUserAndSession(latitude, longitude) {
-        console.log(latitude, longitude);
+        console.log("latitude: " + latitude);
+        console.log("longitude: " + longitude);
+        setButtonDisabled(true);
         let result = false;
         let errorMessage = 'Unknown error';
         await Api.createUserAndSession(latitude, longitude, rangeValue, priceLevel)
@@ -117,16 +128,21 @@ function SessionCreate() {
 
         //push your Json Data in the array
         data.map((markerJson) => dataArray.push(markerJson));
-    
+
         //Loop through the dataArray to create a marker per data using the coordinates in the json
         for (let i = 0; i < dataArray.length; i++) {
-          let marker = new maps.Marker({
+            let marker = new maps.Marker({
             position: { lat: dataArray[i].lat, lng: dataArray[i].lng },
             map,
             label: dataArray[i].id,
-          });
+            });
         }
-      };
+    };
+
+    function handleBoundsChanged() {
+        const mapCenter = refMap.current.getCenter(); //get map center
+        setMarkerCenter(mapCenter);
+    };
 
     return (
         <Box>
@@ -137,43 +153,51 @@ function SessionCreate() {
             <GoogleMapReact
                 bootstrapURLKeys={{ key: GOOGLE_MAPS_API_KEY }}
                 defaultCenter={{ lat: 40.756795, lng: -73.954298 }}
+                center={{lat: currentLocation.lat, lng: currentLocation.lng}}
                 defaultZoom={10}
                 yesIWantToUseGoogleMapApiInternals
                 onGoogleApiLoaded={({ map, maps }) => ModelsMap(map, maps)}
-            ></GoogleMapReact>
+                ref={refMap}
+                onBoundsChanged={handleBoundsChanged}
+            >
+                <CaretDown
+                    color='brand'
+                    position={markerCenter}
+                />
+            </GoogleMapReact>
             </div>    
 
-                <div className={"inner-focus-padding"}>Max Distance: <span
-                    style={{"textAlign": "right"}}>{rangeValue} mi</span></div>
+            <div className={"inner-focus-padding"}>Max Distance: <span
+                style={{"textAlign": "right"}}>{rangeValue} mi</span></div>
 
-                <RangeInput
-                    color='dark'
-                    value={range}
-                    max={8}
-                    min={0}
-                    onChange={event => calcValue(event)}
+            <RangeInput
+                color='dark'
+                value={range}
+                max={8}
+                min={0}
+                onChange={event => calcValue(event)}
+            />
+
+            <RadioButtonGroup
+                className={"menu-radio-group"}
+                label={"Price Range: "}
+                name="priceRange"
+                options={['$','$$','$$$']}
+                value={priceLevel}
+                onChange={(event) => setPriceLevel(event.target.value)}
+            />
+
+            <br/>
+            <div className="center" style={{"marginBottom": "4px"}}>
+                <Button
+                    label={buttonDisabled ? 'Loading...' : "Start a group decision"}
+                    disabled={buttonDisabled}
+                    onClick={() => {
+                        createUserAndSession(currentLocation.lat, currentLocation.lng)
+                    }}
+                    primary={true}
                 />
-
-                <RadioButtonGroup
-                    className={"menu-radio-group"}
-                    label={"Price Range: "}
-                    name="priceRange"
-                    options={['$','$$','$$$']}
-                    value={priceLevel}
-                    onChange={(event) => setPriceLevel(event.target.value)}
-                />
-
-                <br/>
-                <div className="center" style={{"marginBottom": "4px"}}>
-                    <Button
-                        label={buttonDisabled ? 'Loading...' : "Start a group decision"}
-                        disabled={buttonDisabled}
-                        onClick={() => {
-                            getLocation()
-                        }}
-                        primary={true}
-                    />
-                </div>
+            </div>
             </div>
             <br/>
             <br/>
